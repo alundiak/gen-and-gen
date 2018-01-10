77d0d36eb43ced4a1dfa2c171f338d6e5f8c5b3c
Andrii Lundiak <landike@gmail.com>
2018-01-10 23:22:52 +0100

    Initial commit of codebase after extraction from private bitbucket repository.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# gentree
+# Genealogy Tree #
 Genealogy and Genetics related projects sandbox
+
+
+Based on D3.JS try to implement tree visualization
+
+* http://bl.ocks.org/mbostock/4339083
+* http://bl.ocks.org/d3noob/8329447
+* http://techslides.com/over-1000-d3-js-examples-and-demos
+
+## Main D3 dependencies
+- [D3.Cloud](https://github.com/jasondavies/d3-cloud)
+- mayeb [D3.Cloud v4](https://www.npmjs.com/package/d3-v4-cloud)
+- [D3.Scale](https://github.com/d3/d3-scale)
+
+Another way:
+
+* http://coodict.github.io/javascript-in-one-pic/
+
+
+Surnames and Locations Clouds
+
+* http://jsfiddle.net/adiioo7/rutpj/light/
+* https://www.jasondavies.com/wordcloud
+* https://www.npmjs.com/package/d3.layout.cloud
---
+++ js/index.js
@@ -0,0 +1,66 @@
+// https://www.jasondavies.com/wordcloud/
+
+//
+// ES6 doesn't load this:
+// Failed to load module script: The server responded with a non-JavaScript MIME type of "application/json". 
+// Strict MIME type checking is enforced for module scripts per HTML spec.
+// 
+// import {surnamesData} from '/data/surnames.json'
+// import {locationsData} from '/data/locations.json'
+
+import {surnamesData} from '/js/surnames.js'
+import {locationsData} from '/js/locations.js'
+
+// var fill = d3.scale.category20(); // old code
+var fill = d3.schemeCategory20; // new code
+
+console.log("My Surnames Length: "+surnamesData.length);
+console.log("My Locations Length: "+locationsData.length);
+
+var sizeIndex = 20; //50
+var rotateIndex = 0; //90
+var cloudDensitySize = [400, 400]; //[300, 300]
+
+d3.layout.cloud().size(cloudDensitySize)
+    .words(surnamesData.map(function(d) {
+      return {text: d, size: 10 + Math.random() * sizeIndex};
+    }))
+    .rotate(function() { return ~~(Math.random() * 2) * rotateIndex; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
+
+d3.layout.cloud().size(cloudDensitySize)
+    .words(locationsData.map(function(d) {
+      return {text: d, size: 10 + Math.random() * sizeIndex};
+    }))
+    .rotate(function() { return ~~(Math.random() * 2) * rotateIndex; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
+
+
+function draw(words) {
+
+  console.log("Selected words to draw. Length: "+words.length);
+
+  d3.select("body").append("svg")
+      .attr("width", 400)
+      .attr("height", 500)
+    .append("g")
+      .attr("transform", "translate(200,200)")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill[i]; })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
+
