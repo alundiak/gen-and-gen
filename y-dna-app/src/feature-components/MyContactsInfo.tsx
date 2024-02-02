6c87df45a89da936330d074d63de0867774d6d0b
Andrii Lundiak <landike@gmail.com>
2024-02-02 14:25:21 +0100

    Modified a bit

--- y-dna-app/src/feature-components/MyContactsInfo.tsx
+++ y-dna-app/src/feature-components/MyContactsInfo.tsx
@@ -4,8 +4,7 @@ export function MyContactsInfo() {
     <h3>My contact info</h3>
     <div className="links">
       <div className="facebook">
-        <span>My Facebook profile</span>
-        <a href="https://www.facebook.com/andrii.lundiak" target="_blank">Andrii Lundiak</a>
+        <a href="https://www.facebook.com/andrii.lundiak" target="_blank">My Facebook profile</a>
       </div>
       <div className="blog">My blog - <a href="https://lundiak.wordpress.com/" target="_blank">LUND-IA-K</a></div>
       <div className="github">My github - <a href="https://alundiak.github.io/gen-and-gen" target="_blank">Gen&Gen</a> project</div>
